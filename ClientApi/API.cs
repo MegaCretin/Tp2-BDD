d792f8329e2d3f7731746599f7dd81a840c594e8
MegaCretin <megacretin@gmail.com>
2023-03-02 22:03:00 +0100

    Initial Commit

---
+++ ClientApi/API.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ElectionResultCalculator
+{
+    public class ElectionResult
+    {
+        private List<Candidat> _candidats = new List<Candidat>();
+        private List<int> _candidatVotes = new List<int>();
+
+        public class Candidat
+        {
+            public string Name;
+            public int Votes;
+            public int Id;
+
+            public Candidat(int id, string name, int votes)
+            {
+                Id = id;
+                Name = name;
+                Votes = votes;
+            }
+        }
+
+        public List<int> CreerListCandidats(int nbCandidats)
+        {
+            for (int i = 1; i <= nbCandidats; i++)
+            {
+                var candidat = new Candidat(id: i, name: $"Candidat {i}", votes: 0);
+                _candidats.Add(candidat);
+                _candidatVotes.Add(0);
+            }
+
+            return _candidatVotes;
+        }
+
+        public void AjouterVotePourCandidat(int candidatId, int percentage)
+        {
+            var candidat = _candidats.SingleOrDefault(c => c.Id == candidatId);
+            if (candidat != null)
+            {
+                candidat.Votes += percentage;
+                _candidatVotes = _candidats.Select(c => c.Votes).ToList();
+                _candidats = _candidats.OrderByDescending(c => c.Votes).ToList();
+            }
+            else
+            {
+                candidat.Votes += 0;
+            }
+        }
+
+        public int CalculerElectionResultat(List<int> nbVotesOfCandidats)
+        {
+            for (int i = 0; i < nbVotesOfCandidats.Count; i++)
+            {
+                _candidats[i].Votes = nbVotesOfCandidats[i];
+            }
+
+            return _candidats[0].Id;
+        }
+
+        public List<int> CandidatsInSecondRound()
+        {
+            List<int> candidats = new List<int>();
+            List<int> sortedVotes = _candidatVotes.OrderByDescending(v => v).ToList();
+            int maxVotes = sortedVotes[0];
+            int secondMaxVotes = sortedVotes[1];
+
+            for (int i = 0; i < _candidatVotes.Count; i++)
+            {
+                if (_candidatVotes[i] == maxVotes)
+                {
+                    candidats.Add(i + 1);
+                }
+                else if (_candidatVotes[i] == secondMaxVotes)
+                {
+                    candidats.Add(i + 1);
+                }
+            }
+
+            return candidats;
+        }
+
+
+        public static bool SecondRoundRequis(List<int> votes)
+        {
+            int totalVotes = votes.Sum();
+            int maxVotes = votes.Max();
+
+            if (maxVotes > totalVotes / 2)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static List<int> GetCandidatsForSecondRound(List<int> votes)
+        {
+            List<int> candidats = new List<int>();
+            int maxVotes = votes.Max();
+
+            for (int i = 0; i < votes.Count; i++)
+            {
+                if (votes[i] == maxVotes)
+                {
+                    candidats.Add(i + 1);
+                }
+            }
+
+            return candidats;
+        }
+
+        public bool ShouldHoldSecondRound()
+        {
+            return ElectionResult.SecondRoundRequis(_candidatVotes);
+        }
+    }
+}
---
+++ SpecFlowAPI/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using TechTalk.SpecFlow;
+using System.Reflection.Metadata.Ecma335;
+using FluentAssertions;
+using ElectionResultCalculator;
+
+namespace ElectionResultCalculation.Specs
+{
+    [Binding]
+    
+    public class ElectionResultCalculationSteps
+    {
+
+        private List<int> CandidatsVotes;
+        private  int winner;
+        private List<ElectionResult.Candidat> _candidats;
+        private bool _electionClosed;
+        private int _candidatId;
+        private int _percentage;
+        ElectionResult electionResult = new ElectionResult();
+
+        [Given(@"the election has closed")]
+        public void GivenTheElectionHasClosed()
+        {
+            _electionClosed = true;
+        }
+        [Given(@"there are (.*) candidates")]
+        public void GivenThereAreCandidates(int nbCandidates)
+        {
+            if (_electionClosed==true)
+            {
+                CandidatsVotes = electionResult.CreerListCandidats(nbCandidates);
+            } 
+        }
+        [Given(@"candidat (.*) has (.*)% of the votes")]
+        public void GivenCandidateHasOfTheVotes(int candidatId, int percentage)
+        {
+            _candidatId = candidatId;
+            _percentage = percentage;
+            electionResult.AjouterVotePourCandidat(_candidatId, _percentage);
+        }
+        [Given(@"the second round of election has closed")]
+        public void GivenTheSecondRoundOfElectionHasClosed()
+        {
+            _electionClosed = true;
+        }
+
+        [Given(@"both candidats have (.*)% of the votes each")]
+        public void GivenBothCandidatesHaveOfTheVotesEach(int percentage)
+        {
+            electionResult.CreerListCandidats(2);
+            electionResult.AjouterVotePourCandidat(1, percentage);
+            electionResult.AjouterVotePourCandidat(2, percentage);
+        } 
+
+
+        [When(@"I calculat the result of the election")]
+        public void WhenICalculateTheResultOfTheElection()
+        {
+            winner= electionResult.CalculerElectionResultat(CandidatsVotes);
+        }
+        
+        [When(@"I calculat the result of the second round of election")]
+        public void WhenICalculateTheResultOfTheSecondRoundOfElection()
+        {
+            winner = electionResult.CalculerElectionResultat(CandidatsVotes);
+
+        }
+
+        [Then(@"candidat (.*) should be declared the winner")]
+        public void ThenCandidateShouldBeDeclaredTheWinner(int candidate)
+        {
+            winner.Should().Be(candidate); 
+        }
+        [Then(@"a second round of election should be held")]
+        public void ThenASecondRoundOfElectionShouldBeHeld()
+        {
+            electionResult.ShouldHoldSecondRound().Should().BeTrue();
+        }
+
+        [Then(@"only candidat (.*) and (.*) should participate in the second round")]
+        public void ThenOnlyCandidatAndShouldParticipateInTheSecondRound(int candidat1, int candidat2)
+        {
+            electionResult.CandidatsInSecondRound().Should().BeEquivalentTo(new List<int> { candidat1, candidat2 });
+            electionResult.CandidatsInSecondRound().Count.Should().Be(2);
+        }
+        [Then(@"no winner can be determined")]
+        public void ThenNoWinnerCanBeDetermined()
+        {
+            winner.Should().Be(0);
+        }
+    }
+
+
+}
